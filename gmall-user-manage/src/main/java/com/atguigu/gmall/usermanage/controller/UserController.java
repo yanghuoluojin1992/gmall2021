a5b1442a359256d7ce8fddbd2e30cd312d4a3431
yanghuoluojin1992 <74457003+yanghuoluojin1992@users.noreply.github.com>
2021-02-01 18:37:31 +0800

    Update UserController.java
    云端直接修改了UserController.java

--- gmall-user-manage/src/main/java/com/atguigu/gmall/usermanage/controller/UserController.java
+++ gmall-user-manage/src/main/java/com/atguigu/gmall/usermanage/controller/UserController.java
@@ -23,6 +23,7 @@ public class UserController {
 
     @RequestMapping("findAllList")
     public List<UserInfo> findAllList(){
+        System.out.println("云端修改");
         return userService.findAll();
     }
 }
